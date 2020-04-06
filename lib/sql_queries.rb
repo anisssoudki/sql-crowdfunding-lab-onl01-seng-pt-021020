ab50d99f8e7f85edd4993b17ac5da7569cdd3973
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-04-06 03:31:06 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -21,7 +21,7 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "select projects.title, (projects.funding_goal - sum(pledges.amout)) * -1 as over_goal from projects
+  "select projects.title, (projects.funding_goal - sum(pledges.amout)) * -1 AS over_goal from projects
   inner join pledges
   on projects.funding_goal = pledges.amount 
   group by projects.title"
